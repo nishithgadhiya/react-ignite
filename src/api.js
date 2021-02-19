14ec78fe4b139fe7a2d4a40bdc76ad92fbcb1098
Nishith Gadhiya <nishithgadhiya@gmail.com>
2021-02-19 09:22:08 +0530

    setup redux

--- src/api.js
+++ src/api.js
@@ -1,29 +1,10 @@
 //Base URL
 const base_url = `https://api.rawg.io/api/`;
 
-//Getting the date
-const getCurrentMonth = () => {
-  const month = new Date().getMonth() + 1;
-  if (month < 10) {
-    return `0${month}`;
-  } else {
-    return month;
-  }
-};
-
-const getCurrentDay = () => {
-  const day = new Date().getDate();
-  if (day < 10) {
-    return `0${day}`;
-  } else {
-    return day;
-  }
-};
-
 //Current day/month/year
 const currentYear = new Date().getFullYear();
-const currentMonth = getCurrentMonth();
-const currentDay = getCurrentDay();
+const currentMonth = ("0" + (new Date().getMonth() + 1)).slice(-2);
+const currentDay = ("0" + new Date().getDate()).slice(-2);
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
@@ -31,6 +12,4 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular games
 const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}?dates=${lastYear},${currentYear}&ordering=-rating&page_size=10`;
 
-const popularGamesURL = () => `${base_url}${popular_games}`;
-
-console.log(popularGamesURL());
+export const popularGamesURL = () => `${base_url}${popular_games}`;
