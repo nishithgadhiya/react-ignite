cf8ef97a27d674f92d828f2f577d4b0b74655a9e
Nishith Gadhiya <nishithgadhiya@gmail.com>
2021-02-24 13:16:30 +0530

    updated file

--- src/create-env.js
+++ src/create-env.js
@@ -1,2 +1,5 @@
 const fs = require("fs");
-fs.writeFileSync("./.env", `API_KEY=${process.env.REACT_APP_RAWG_API}\n`);
+fs.writeFileSync(
+  "./.env",
+  `REACT_APP_RAWG_API=${process.env.REACT_APP_RAWG_API}\n`
+);
